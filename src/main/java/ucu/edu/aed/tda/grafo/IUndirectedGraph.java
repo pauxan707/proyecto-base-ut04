3bd9e216f1f1c5dcc42d8cbdac05d336d5a73321
agusberrutti <agustina.berrutti@correo.ucu.edu.uy>
2026-06-08 18:57:28 -0300

    UndirectedGraph listo

--- src/main/java/ucu/edu/aed/tda/grafo/IUndirectedGraph.java
+++ src/main/java/ucu/edu/aed/tda/grafo/IUndirectedGraph.java
@@ -4,4 +4,7 @@ package ucu.edu.aed.tda.grafo;
 import ucu.edu.aed.tda.grafo.model.IGraph;
 
 public interface IUndirectedGraph<V, D> extends IGraph<V, D> {
+    /**
+     * Está definida en IGraph, y la implementación de UndirectedGraph garantiza la semántica no dirigida
+     */
 }
